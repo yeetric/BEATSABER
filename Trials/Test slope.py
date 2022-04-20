3f800391761adfb5331ac8810808b934356596ce
yeetric <82407170+yeetric@users.noreply.github.com>
2022-04-20 13:42:11 -0700

    Added timelimit. TO-DO:
    1. Add sound after touch
    2. Add comments to code
    3. 3d game? maybe increase rectangle size everytime smth

---
+++ Trials/Test slope.py
@@ -0,0 +1,12 @@
+
+x = (30,60)
+y = (10,30)
+
+slope = (y[1]-x[1])/(y[0]-x[0])
+b = -1 * (slope*y[0]-y[1])
+print (f"y = {slope}x + {b}")
+
+
+
+# This can be used to find the extents of a line to plot
+#  so that the bar the person is hold faces the direction of his hand
--- Trials/test slope entire.py
+++ Trials/test slope entire.py
@@ -22,47 +22,45 @@ def RNG(a, b):
 
 def main():
     score = 0
-    BLUEBoxReset= 50
+    blueBoxReset= 50
     redBoxReset = 50
     while True:
-        BLUEtouching = False
-        REDtouching = False
+        touching = False
         success, img = cap.read()
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
-        while not BLUEtouching or not REDtouching:
+        while not touching:
             success, img = cap.read()
             imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             boxSize = 50
             cv2.putText(img, str(int(score)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
 
             # cv2.rectangle(img, (0, 0), (widthCam, heightCam), (0, 0, 0), cv2.FILLED) # black background
-
             results = pose.process(imgRGB)
             # print(results.pose_landmarks)
 
             if results.pose_landmarks:
-                # mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS) #overall pose
+                mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS) #overall pose
                 for id, lm in enumerate(results.pose_landmarks.landmark):
                     h, w, c = img.shape
                     # print(id, lm)
                     cx, cy = int(lm.x * w), int(lm.y * h)
                     # cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
                     if id == 20 or id == 19:
-                        if BLUEBoxReset >= 50:
+                        if blueBoxReset >= 50:
                             BLUEcoor1 = (RNG(0, widthCam - boxSize), RNG(0, heightCam - boxSize))
                             BLUEcoor2 = (BLUEcoor1[0] + boxSize, BLUEcoor1[1] + boxSize)
                             BLUEmid = (int((BLUEcoor1[0] + BLUEcoor2[0]) / 2), int((BLUEcoor1[1] + BLUEcoor2[1]) / 2))
-                            BLUEBoxReset = 0
-                        BLUEBoxReset += 1
+                            blueBoxReset = 0
+                        blueBoxReset += 1
 
                         if redBoxReset >= 50:
                             REDcoor1 = (RNG(0, widthCam - boxSize), RNG(0, heightCam - boxSize))
                             REDcoor2 = (REDcoor1[0] + boxSize, REDcoor1[1] + boxSize)
                             REDmid = (int((REDcoor1[0] + REDcoor2[0]) / 2), int((REDcoor1[1] + REDcoor2[1]) / 2))
-                            redBoxReset = 0
-                        redBoxReset += 1
+                            REDBoxReset = 0
+                        REDBoxReset += 1
 
                         print(id, cx,cy)
                         if id == 20:
@@ -71,21 +69,15 @@ def main():
 
                             if BLUElength < boxSize+50:
                                 score += 1
-                                BLUEtouching = True
-                                BLUEBoxReset = 300
-                        elif id == 19:
+                                touching = True
+                                blueBoxReset = 300
+                        else:
                             cv2.line(img, (cx, cy), (cx, cy-100), (0, 0, 255), 10)
-                            REDlength = math.hypot(cx - REDmid[0], cy - REDmid[1])
-
-                            if REDlength < boxSize + 50:
-                                score += 1
-                                REDtouching = True
-                                redBoxReset = 100
+                    else:
+                        pass
                     try:
                         cv2.circle(img, BLUEmid, 5, (255, 0, 0), cv2.FILLED)
                         cv2.rectangle(img, BLUEcoor1, BLUEcoor2, (255, 0, 0), 2)
-                        cv2.circle(img, REDmid, 5, (0, 0, 255), cv2.FILLED)
-                        cv2.rectangle(img, REDcoor1, REDcoor2, (0, 0, 255), 2)
                     except:
                         print("hand off screen")
             cv2.imshow("Image", cv2.flip(img,1))
